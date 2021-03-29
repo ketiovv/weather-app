f666e8fdc7b183d6a9e6c414883c5731de6be11d
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-29 16:08:48 +0200

    implemented App.js to display new components

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { BottomNavigation, Text } from 'react-native-paper';
+import Location from './components/Location';
+import WeatherViewContainer from './components/WeatherViewContainer';
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const NormalViewRoute = () => <WeatherViewContainer olderPeopleView={false} />
+  const OlderPeopleRoute = () => <WeatherViewContainer olderPeopleView={true} />
+  const LocationRoute = () => <Location/>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'normal', title: 'Normal people', icon: 'account' },
+    { key: 'older', title: 'Older people', icon: 'magnify-plus' },
+    { key: 'location', title: 'Location', icon: 'map-marker' }
+  ])
+    const renderScene = BottomNavigation.SceneMap({
+        normal: NormalViewRoute,
+        older: OlderPeopleRoute,
+        location: LocationRoute
+    });
+
+    return (
+        <BottomNavigation 
+            navigationState={{index, routes}}
+            onIndexChange={setIndex}
+            renderScene={renderScene} />
+      );
+}
