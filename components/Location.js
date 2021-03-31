f5e0c6a4877a16a54c61337b8f10025b7cc7a798
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-31 14:05:04 +0200

    designed 'choose location' view

--- components/Location.js
+++ components/Location.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
-import { TextInput, Button, Title, Subheading } from "react-native-paper";
+import {
+  TextInput,
+  Button,
+  Title,
+  Subheading,
+  useTheme,
+} from "react-native-paper";
 import { API_KEY } from "../utils/constants";
 
 const Location = (props) => {
@@ -16,33 +22,47 @@ const Location = (props) => {
         }
       });
   };
+  const { colors } = useTheme();
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.accent,
+      paddingHorizontal: 10,
+      justifyContent: "center",
+      alignContent: "center",
+    },
+    currentLocation: {
+      marginBottom: 30,
+      color: "#fff",
+    },
+    btnSubmit: {
+      marginTop: 10,
+      marginHorizontal: "30%",
+    },
+    fontColor: {
+      color: "#fff",
+    },
+  });
 
   return (
     <View style={styles.container}>
-      <Subheading>Current location</Subheading>
+      <Subheading style={styles.fontColor}>Current location</Subheading>
       <Title style={styles.currentLocation}>{props.selectedLocation}</Title>
-      <Subheading>Enter new location</Subheading>
+      <Subheading style={styles.fontColor}>Enter new location</Subheading>
       <TextInput
         label="Location"
         value={text}
         onChangeText={(text) => setText(text)}
       />
-      <Button onPress={() => handleChangeLocation()}>Submit</Button>
+      <Button
+        onPress={() => handleChangeLocation()}
+        mode={"contained"}
+        style={styles.btnSubmit}
+      >
+        Submit
+      </Button>
     </View>
   );
 };
 
 export default Location;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    marginHorizontal: 10,
-    justifyContent: "center",
-    alignContent: "center",
-  },
-  currentLocation: {
-    marginBottom: 30,
-  },
-});
