e2169e45219c1ea01f9fb2fe4ee792c7fbc4cefb
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-03-31 14:04:02 +0200

    created functions which displays correct form of date and time

--- utils/utils.js
+++ utils/utils.js
@@ -1,20 +1,71 @@
 export function getWeekName(number) {
     switch (number) {
         case 1:
-            return 'Monday'
+            return "Monday";
         case 2:
-            return 'Tuesday'
+            return "Tuesday";
         case 3:
-            return 'Wednesday'
+            return "Wednesday";
         case 4:
-            return 'Thursday'
+            return "Thursday";
         case 5:
-            return 'Friday'
+            return "Friday";
         case 6:
-            return 'Saturday'
+            return "Saturday";
         case 7:
-            return 'Sunday'
+            return "Sunday";
         default:
             break;
+    }
+}
+
+export function getIconForWeather(weather) {
+    switch (weather) {
+        case "Rain":
+            return "weather-pouring";
+        case "Clouds":
+            return "weather-cloudy";
+        case "Fog":
+            return "weather-fog";
+        case "Clear":
+            return "white-balance-sunny";
+        case "Snow":
+            return "snowflake";
+        default:
+            return "cloud";
+    }
+}
+
+export function getCorrectDateString(date) {
+    let day = date.getUTCDate();
+    if (day < 10) {
+        day = "0" + day;
+    }
+    let month = date.getUTCMonth();
+    if (month < 10) {
+        month = "0" + month;
+    }
+    let year = date.getUTCFullYear();
+
+    return `${day}.${month}.${year}`;
+}
+
+export function getCorrectTimeString(date, style) {
+    let hours = date.getUTCHours();
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    let minutes = date.getUTCMinutes();
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    let seconds = date.getUTCSeconds();
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    if (style == "short") {
+        return `${hours}:${minutes}`;
+    } else {
+        return `${hours}:${minutes}:${seconds}`;
     }
 }
